1d4aff979f2d14396808b90f43a5f444af52947f
Milan van Dijck <miscoriadev@gmail.com>
2017-06-02 10:59:39 +0200

    Sorted activities

--- routes_apiv1.js
+++ routes_apiv1.js
@@ -8,7 +8,6 @@ var dbConnection;
 
 router.post('/activiteiten', function (req, res) {
     var update_date = req.body.update_date ||  '';
-    var sort = req.body.sort ||  'false';
 
     if (update_date == '') {
         console.log("Incompatible date format");
@@ -32,14 +31,82 @@ router.post('/activiteiten', function (req, res) {
             }
         });
 
-        try {
-            dbConnection.query('SELECT * FROM SVVirgo.activities WHERE updated > \''+update_date+'\' ORDER BY start DESC', function (err, rows, fields) {
-                if (err) throw err;
+            try {
+                dbConnection.query('SELECT * FROM SVVirgo.activities WHERE updated > \'' + update_date + '\' ORDER BY start DESC', function (err, rows, fields) {
+                    if (err) throw err;
 
-                var response = [];
+                    var response = [];
+
+                    rows.forEach(function (row) {
+                        console.log("Activity: " + row.title.toString());
+
+                        var activity = {
+                            id: row.id.toString(),
+                            name: row.title.toString(),
+                            description: row.description.toString(),
+                            image: row.image.toString(),
+                            startDate: row.start.toString(),
+                            endDate: row.end.toString(),
+                            price: row.price.toString(),
+                            facebook: row.facebook.toString(),
+                            location: row.location.toString()
+                        };
+                        response.push(activity);
+                    });
+
+                    //console.log("JSON: " + JSON.stringify(response));
+
+                    res.status(200);
+                    res.json(response);
+                });
+            } catch (err) {
+                console.log("Database timeout error");
+                res.status(500);
+                res.json({
+                    "status": 500,
+                    "message": "Database timeout error."
+                });
+                throw err;
+            }
+
+        dbConnection.end();
+    }
+});
+
+router.get('/activiteiten/sort', function (req, res) {
+    var events_after = req.body.events_after ||  '';
+
+    if (update_date == '') {
+        console.log("Incompatible date format");
+        res.status(500);
+        res.json({
+            "status": 500,
+            "message": "Incompatible date format"
+        });
+    } else {
+        dbConnection = sql.createConnection({
+            host: settings.dbHost,
+            user: settings.dbUser,
+            password: settings.dbPassword,
+            dateStrings: 'date'
+        });
+        dbConnection.connect(function (err) {
+            if (!err) {
+                console.log("Database is connected ... nn");
+            } else {
+                console.log("Error connecting database ... nn");
+            }
+        });
+
+        var response = [];
+
+        try {
+            dbConnection.query('SELECT * FROM SVVirgo.activities WHERE end >= CURRENT_DATE() ORDER BY start ASC', function (err, rows, fields) {
+                if (err) throw err;
 
                 rows.forEach(function (row) {
                     console.log("Activity: " + row.title.toString());
+                    var upcoming = [];
 
                     var activity = {
                         id: row.id.toString(),
@@ -52,24 +119,59 @@ router.post('/activiteiten', function (req, res) {
                         facebook: row.facebook.toString(),
                         location: row.location.toString()
                     };
-                    response.push(activity);
+
+                    upcoming.push(activity);
                 });
-
-                //console.log("JSON: " + JSON.stringify(response));
-
-                res.status(200);
-                res.json(response);
+                response.push(upcoming)
             });
-        } catch (err){
-                console.log("Database timeout error");
-                res.status(500);
-                res.json({
-                    "status": 500,
-                    "message": "Database timeout error."
-                });
-                throw err;
+        } catch (err) {
+            console.log("Database timeout error");
+            res.status(500);
+            res.json({
+                "status": 500,
+                "message": "Database timeout error."
+            });
+            throw err;
         }
-        dbConnection.end();
+
+        try {
+            dbConnection.query('SELECT * FROM SVVirgo.activities WHERE end < CURRENT_DATE() AND end > \''+ events_after +'\' ORDER BY start DESC', function (err, rows, fields) {
+                if (err) throw err;
+
+                rows.forEach(function (row) {
+                    console.log("Activity: " + row.title.toString());
+                    var past = [];
+
+                    var activity = {
+                        id: row.id.toString(),
+                        name: row.title.toString(),
+                        description: row.description.toString(),
+                        image: row.image.toString(),
+                        startDate: row.start.toString(),
+                        endDate: row.end.toString(),
+                        price: row.price.toString(),
+                        facebook: row.facebook.toString(),
+                        location: row.location.toString()
+                    };
+
+                    past.push(activity);
+                });
+                response.push(past)
+            });
+        } catch (err) {
+            console.log("Database timeout error");
+            res.status(500);
+            res.json({
+                "status": 500,
+                "message": "Database timeout error."
+            });
+            throw err;
+        }
+
+        //console.log("JSON: " + JSON.stringify(response));
+
+        res.status(200);
+        res.json(response);
     }
 });
 
